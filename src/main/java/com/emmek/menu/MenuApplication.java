e7b838b16b1211d1867db1c2bb66e2fa3cebf8b6
Matteo Falasca <matteo.falasca@emmek.org>
2023-10-30 16:54:24 +0100

    fix

--- src/main/java/com/emmek/menu/MenuApplication.java
+++ src/main/java/com/emmek/menu/MenuApplication.java
@@ -10,31 +10,6 @@ public class MenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MenuApplication.class, args);
-
-//        Consumation pizzaMargherita = new PizzaMargherita();
-//
-//        Consumation pizza = new PizzaMargherita();
-//
-//        Consumation pizzaS = new PizzaMargherita();
-//        Consumation pizzaSalami = new ExtraSalameDecorator(pizzaS);
-//        pizzaS.setProductName("Salami Pizza");
-//
-//        Consumation pizzaA = new PizzaMargherita();
-//        Consumation pizzaAnanas = new ExtraAnanasDecorator(pizzaA);
-//        Consumation pizzaHawaii = new ExtraProsciuttoDecorator(pizzaAnanas);
-//        pizzaA.setProductName("Hawaiian Pizza");
-//
-//        System.out.println(pizzaHawaii.toString());
-//        System.out.println(pizzaSalami.toString());
-//
-//        List<Consumation> pizzas = List.of(pizza, pizzaHawaii, pizzaSalami);
-//        List<Consumation> drinks = List.of(new CocaCola(), new Birra(), new Acqua());
-//        List<Consumation> toppings = List.of(new ExtraSalameDecorator(pizza), new ExtraAnanasDecorator(pizza), new ExtraProsciuttoDecorator(pizza));
-//
-//        System.out.println(pizza.toString());
-//
-//        Menu menu = new Menu(pizzas, drinks, toppings);
-
         configurationClass();
 
     }
